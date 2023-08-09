695e0fbebcfbd90635add734a369a04ce53bd51c
Super-Star-Corp <e.141592653589793238462pi@gmail.com>
2023-08-09 20:23:38 +1000

    Beta 1.2
    -fixed custom dice number
    -other small changes

--- R/rng.js
+++ R/rng.js
@@ -1,8 +1,8 @@
 let ds = 6
+document.getElementById("cbt").onclick = function(){
+    ds = Number(window.prompt("How Many Sides Shoud The Dice Have?"))
+}
 document.getElementById("Bt").onclick = function(){
     let rn = Math.floor(Math.random() * ds + 1);
     document.getElementById("rnt").innerHTML = "The Random Number Is " + rn;
-}
-document.getElementById("cbt").onclick = function(){
-    let ds = Number(window.prompt("How Many Sides Shoud The Dice Have?"))
 }
